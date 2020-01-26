28e0b8cb7a0c0c1a8a2466278cb4f8641a5967b0
johnebri <john.ebri@yahoo.com>
2020-01-26 15:14:53 +0000

    junit annotations

--- test/com/in28minutes/junit/MyMathTest.java
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -2,6 +2,10 @@ package com.in28minutes.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
@@ -9,15 +13,36 @@ class MyMathTest {
 	// 1,2,3 => 6
 	MyMath myMath = new MyMath(); 
 	
+	@Before
+	public void before() {
+		System.out.println("Before");
+	}
+	
+	@After
+	public void after() {
+		System.out.println("After");
+	}
+	
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before Class");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After Class");
+	}
+	
 	@Test
 	public void sum_with3numbers() {
-		
+		System.out.println("First Test");
 		// Check that the result is 6
 		assertEquals(6, myMath.sum(new int[] {1,2,3}));
 	}
 	
 	@Test
 	public void sum_with1number() {
+		System.out.println("Second Test");
 		// Check that the result is 6
 		assertEquals(3, myMath.sum(new int[] {3}));
 	}
