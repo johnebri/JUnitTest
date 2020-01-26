e6eba2e51ed9a26d969bf54f6d68966f5fb77a6d
johnebri <john.ebri@yahoo.com>
2020-01-26 14:31:03 +0000

    test execution

--- test/com/in28minutes/junit/MyMathTest.java
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -5,9 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
-
+	// MyMath.sum
+	// 1,2,3 => 6
+	MyMath myMath = new MyMath(); 
+	
 	@Test
-	void test() {
+	public void sum_with3numbers() {
+		
+		// Check that the result is 6
+		assertEquals(6, myMath.sum(new int[] {1,2,3}));
+	}
+	
+	@Test
+	public void sum_with1number() {
+		// Check that the result is 6
+		assertEquals(3, myMath.sum(new int[] {3}));
 	}
 
 }
