4de6d8fbaaf0497d57631cc5ac926b95db40ae92
johnebri <john.ebri@yahoo.com>
2020-01-26 14:52:50 +0000

    assertions

---
+++ test/com/in28minutes/junit/AssertTest.java
@@ -0,0 +1,18 @@
+package com.in28minutes.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AssertTest {
+
+	@Test
+	public void test() {
+// 		assertEquals(1, 2);
+//		assertTrue(condition);
+//		assertFalse(condition);
+//		assertNotNull(value);
+//		assertNull(value);
+	}
+
+}
